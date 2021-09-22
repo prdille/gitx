7f99d5aaa75bb36dc0664dc4d8bc525180edf51d
Paul Dille <pdille@med.umich.edu>
2021-09-22 16:43:23 -0400

    2021-09-22

---
+++ narwhal/chestnuts/scratch/lawdi-dawdi.sh
@@ -0,0 +1,34 @@
+DRIVIN: 
+  - SDS-4042:
+    - Groups API
+      - options
+        - python, ansible, curl with bash, 
+        - python would interact with the api very well
+          architect out our 
+            -
+            - methods
+            - 
+crack oauth token
+tasks
+ - crack oauth token
+ -  groups API action
+	#- postk
+    #- postk
+    #- crack#
+    #- crack#
+    #- crack#
+    #- put
+    #- postk
+    #- postk
+    #- etc
+Philosophical Inquiries into the Essence of Human Freedom
+
+gtan_typ
+scope
+
+TC: manage groups via ansible
+AC: began coding
+OBS:
+  authenticate with oauth
+  "lawdi-dawdi.sh" [New] 27L, 437B written
+
---
+++ narwhal/chestnuts/scratch/scripting.sh
@@ -0,0 +1,4 @@
+TEST
+for i in {0..4};do echo $i;done
+TOUCH
+for i in {0..3};do touch <file.name>$i.txt;done
